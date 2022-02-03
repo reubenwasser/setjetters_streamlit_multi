d8b4432f03bcea9fca84ae4a3c2ccfb00b412a75
rubadub13 <reubenwasser13@gmail.com>
2022-02-02 22:37:20 -0800

    geocoder complete!

--- apps/spatial_attribute_update.py
+++ apps/spatial_attribute_update.py
@@ -1,13 +1,46 @@
+from curses import raw
+from matplotlib.pyplot import table
 import streamlit as st
-from data.create_data import mySQL_query
+import pandas as pd
+from data.create_data import mysql_connection
 from opencage.geocoder import OpenCageGeocode
 
 
 def geocoder(table, selector):
     geocoder = OpenCageGeocode(st.secrets.opencage.api_key)
 
+    cols = ["id"]
+
+    my_bar = st.progress(0.0)
+    progress_counter = 0
+    progress_add = 1 / len(table.index)
+
+    if "FLAG" in selector:
+        cols.append("FLAG")
+    if "TIMEZONE" in selector:
+        cols.append("TIMEZONE")
+    if "CITY" in selector:
+        cols.append("CITY")
+    if "CITY_DISTRICT" in selector:
+        cols.append("CITY_DISTRICT")
+    if "CONTINENT" in selector:
+        cols.append("CONTINENT")
+    if "COUNTRY" in selector:
+        cols.append("COUNTRY")
+    if "CURRENCY" in selector:
+        cols.append("CURRENCY")
+    if "STATE" in selector:
+        cols.append("STATE")
+    if "NEIGHBORHOOD" in selector:
+        cols.append("NEIGHBORHOOD")
+    if "ROAD" in selector:
+        cols.append("ROAD")
+    if "FULL_ADDRESS" in selector:
+        cols.append("FULL_ADDRESS")
+
     for index, row in table.iterrows():
         results = geocoder.reverse_geocode(row.lat, row.lng)
+
         if "FLAG" in selector:
             table.loc[index, 'FLAG'] = results[0].get("annotations").get("flag")
         if "TIMEZONE" in selector:
@@ -18,34 +51,88 @@ def geocoder(table, selector):
             table.loc[index, 'CITY_DISTRICT'] = results[0].get("components").get("city_district")
         if "CONTINENT" in selector:
             table.loc[index, 'CONTINENT'] = results[0].get("components").get("continent")
-        # table.loc[index, 'COUNTRY'] = results[0].get("components").get("country")
-        # table.loc[index, 'CURRENCY'] = results[0].get("components").get("currency")
-        # table.loc[index, 'STATE'] = results[0].get("components").get("state")
-        # table.loc[index, 'NEIGHBORHOOD'] = results[0].get("components").get("neighborhood")
-        # table.loc[index, 'ROAD'] = results[0].get("components").get("road")
-        # table.loc[index, 'FULL_ADDRESS'] = results[0].get("formatted")
+        if "COUNTRY" in selector:
+            table.loc[index, 'COUNTRY'] = results[0].get("components").get("country")
+        if "CURRENCY" in selector:
+            table.loc[index, 'CURRENCY'] = results[0].get("components").get("currency")
+        if "STATE" in selector:
+            table.loc[index, 'STATE'] = results[0].get("components").get("state")
+        if "NEIGHBORHOOD" in selector:
+            table.loc[index, 'NEIGHBORHOOD'] = results[0].get("components").get("neighborhood")
+        if "ROAD" in selector:
+            table.loc[index, 'ROAD'] = results[0].get("components").get("road")
+        if "FULL_ADDRESS" in selector:
+            table.loc[index, 'FULL_ADDRESS'] = results[0].get("formatted")
+        
+        # st.write(f"{index} rows processed")
+
+
+        # for percent_complete in range(len(table.index)):
+        progress_counter = progress_counter + progress_add
+        my_bar.progress(progress_counter)
+        
+    return table[cols]
 
 
 def app():
     st.title('Spatial Attribute Database Update')
+    st.write("Uses geocoder to derive spatial attributes and then populate the setjetters database with these")
 
-    st.write("Uses geocoder to derive spatial attributes and then populate the setjetter database with these")
+    test = st.checkbox('Test (only first 10 rows)')
+    update_db = st.checkbox('Update database')
 
-    query = "SELECT * from real_locations;"
-    df = mySQL_query(query)
+    # get connection
+    conn = mysql_connection()
+    raw_conn = conn.raw_connection()
+
+    # get tabke names
+    cursor=raw_conn.cursor()
+    cursor.execute("SHOW TABLES")
+    table_list = []
+    for table_name in cursor:
+        table_name_fixed = table_name[0]
+        table_list.append(table_name_fixed)
+
+    #select table
+    select_table = st.selectbox(
+     'Which table would you like to retrieve spatial attributes for (From Opencage OSM geocoder)?',
+     table_list)
+    
+    if test == True:
+        query = f"SELECT * FROM {select_table} LIMIT 10;"
+    else:
+        query = f"SELECT * from {select_table};"
+
+    df = pd.read_sql(query, con=conn)
+
+
     st.write(df)
 
-    options = ['FLAG', 'TIMEZONE', 'CITY', 'CITY_DISTRICT', 'CONTINENT']
+    # select attributes
+    options = ['FLAG', 'TIMEZONE', 'CITY', 'CITY_DISTRICT', 'CONTINENT', 'COUNTRY', 'CURRENCY', 'STATE', 'NEIGHBORHOOD', 'ROAD', 'FULL_ADDRESS']
 
     selected_options = st.multiselect(
      'Attributes to add', options, default=options)
 
-    # st.button("Submit query", on_click=geocoder(df, selected_options))
-    if st.button('Get spatial Attributes!!!'):
-        st.write('Geocoding....')
-        geocoder(df, selected_options)
+    selected_options.append("id")
 
-        st.write(df)
+    if 'get_attributes' not in st.session_state:
+        st.session_state.get_attributes_button_clicked = False
+
+    # st.button("Submit query", on_click=geocoder(df, selected_options))
+    if st.button('Get attributes'):
+        st.session_state.get_attributes_button_clicked = True
+        st.write('Geocoding.... This may take a while')
+
+        geocode_df = geocoder(df, selected_options)
+        st.write(geocode_df)
+
+        if update_db:
+            new_table_name = select_table + "_geocode"
+            
+            geocode_df.to_sql(new_table_name, conn, if_exists='replace')
+            st.write(f"{new_table_name} added to the database, join by id onto {select_table} in order to integrate spatial attributes")
+
 
 
 
--- data/create_data.py
+++ data/create_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import mysql.connector
 from sqlalchemy import create_engine
 
 
@@ -10,6 +11,21 @@ def create_table(n=7):
     return df
 
 # TODO change to DB credentials to secret management
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def mysql_connection():
+    # Connection parameters
+    host = st.secrets.mysql.host
+    database = st.secrets.mysql.database
+    user = st.secrets.mysql.user
+    password = st.secrets.mysql.password
+
+    db_connection_str = f"mysql+pymysql://{user}:{password}@{host}/{database}"
+    sql_engine = create_engine(db_connection_str)
+    # db = sql_engine.raw_connection()
+    # db=mysql.connector.connect(host=host, user=user, password=password,database=database)
+    # df = pd.read_sql(query, con=conn)
+    return sql_engine
+
 @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def mySQL_query(query):
     # Connection parameters
@@ -23,4 +39,3 @@ def mySQL_query(query):
     conn = sql_engine.raw_connection()
     df = pd.read_sql(query, con=conn)
     return df
-
